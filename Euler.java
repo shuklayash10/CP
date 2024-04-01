e2a5a6974c435490c7795bf670832e2ea4f2505a
Yash Shukla <cs23mtech14018@iith.ac.in>
2024-04-01 16:20:36 +0530

    Euler1

---
+++ Euler.java
@@ -0,0 +1,16 @@
+
+public class Euler {
+    public static void main(String args[])
+    {
+                        int sum =0;
+                    for (i=3;i<=1000;i++)
+        {
+            if( i%3==0 || i%5==0)
+            {
+                sum=+sum + i;
+            }
+        }
+        system.out.println("sum is");
+}
+
+}
